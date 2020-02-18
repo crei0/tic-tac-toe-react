5c9c7ba13ff62aca541122d478997e2e1deffd65
André Guedes <andre.guedes@staizen.com>
2020-02-18 18:44:18 +0100

    Removed leftover file, tests for Board

---
+++ src/components/Board.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Board from './Board';
+
+describe('Board', () => {
+  test('renders without crashing', () => {
+    const { getByText } = render(<Board />);
+
+    const title = getByText(/The current player is X/i);
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('renders 9 BoardCells', () => {
+    const { container } = render(<Board />);
+
+    // This is a bit brittle
+    const allCells = container.querySelectorAll('.tic-tac-toe__board-cell');
+
+    expect(allCells.length === 9);
+  });
+});
